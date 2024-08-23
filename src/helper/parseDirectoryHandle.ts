c747224b146e9369c606eb7fcea177e8f81b5abe
mueenou <mueen.hossain@gmail.com>
2024-08-23 22:49:18 +0200

    new features: import, export, empty add new responsive view

--- src/helper/parseDirectoryHandle.ts
+++ src/helper/parseDirectoryHandle.ts
@@ -1,18 +1,20 @@
 import { Folder } from "@/types/Folder";
 
-export default async function parseDirectoryHandle(
-  directoryHandle: FileSystemDirectoryHandle & {
-    entries: () => AsyncIterableIterator<[string, FileSystemHandle]>;
-  }
+export async function parseDirectoryHandle(
+  directoryHandle: FileSystemDirectoryHandle
 ): Promise<Folder> {
   const folders: Folder[] = [];
-  for await (const [name, handle] of directoryHandle.entries()) {
+
+  for await (const [name, handle] of (directoryHandle as any).entries()) {
     if (handle.kind === "directory") {
-      const subFolder = await parseDirectoryHandle(handle);
+      const subFolder = await parseDirectoryHandle(
+        handle as FileSystemDirectoryHandle
+      );
       folders.push(subFolder);
     } else if (handle.kind === "file") {
       folders.push({ name });
     }
   }
+
   return { name: directoryHandle.name, folders };
 }
